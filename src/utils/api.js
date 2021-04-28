a7c8dfbd43527f4caca9d07dc2ec570d155d41b9
Aarsh <aarsh937@gmail.com>
2021-04-28 00:13:30 -0400

    https for api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,6 @@ import { OMDB_API_KEY } from "./constants";
 
 export const searchMovie = (query) => {
   return axios.get(
-    `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&type=movie&s=${query}`
+    `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&type=movie&s=${query}`
   );
 };
